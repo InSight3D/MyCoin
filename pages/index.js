6d7a22888aefc9ee9d8455a525bbb2d61f8661c3
sbelka-1703 <sbelka93@gmail.com>
2022-05-10 08:29:58 -0700

    added assets for the homepage, still need styling

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import { useState, useEffect } from 'react'
 import axios from "axios";
 import Navigation from '../components/Header';
 import Footer from '../components/footer';
+import { MailIcon } from '@heroicons/react/solid'
+
 
 
 
 export default function Home() {
-  
+
 
 
 
@@ -18,11 +21,22 @@ export default function Home() {
         <meta property="og:title" content="My Coin" key="title" />
       </Head>
 
-  
-       <div className='pb-2'></div>
+      <div className='flex'>
+        {/* Text and input field */}
+        <div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+          <div className='h-8 w-8 bg-black rounded-full'></div>
+          <MailIcon className="h-8 w-8 text-black hover:text-gray-500 cursor-pointer" />
+          <input className='border-2'>
+          </input>
+        </div>
 
-
-   
+        {/* Logo */}
+        <div>
+          <Image src="/logo.svg" width="300" height="280" alt="logo" />
+        </div>
+      </div>
 
     </>
   );

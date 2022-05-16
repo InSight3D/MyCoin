97bd70a9a417e19245f47c0c59d8652ba84fb19e
Neil Shah <neildshah1013@gmail.com>
2022-05-16 09:28:10 -0400

    Meta Fix

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,10 @@ export default function Home() {
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://insight3d.github.io/MyCoin/public/logo.png" />
         <meta property="og:url" content="http://mycoin.insight3d.tech" />
-        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:card" content="summary_large_image" />
 
         <meta property="og:description" content=" InSight3D MyCoin is a free mock crypto exhange that grants users $10,000 to buy and sell 100+ coins at live market price. The coins as well as money will have no real value, as it is a mock exchange. " />
-        <meta property="og:site_name" content="MyCoin by InSight3D=" />
+        <meta property="og:site_name" content="MyCoin by InSight3D" />
         <meta name="twitter:image:alt" content="MyCoin" />
 
       </Head>

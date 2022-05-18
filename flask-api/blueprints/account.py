684872d24b83d0fb1def99037457b56f08ef3605
Neil Shah <neildshah1013@gmail.com>
2022-05-18 13:12:32 -0400

    Flask API Designed For Auth0

--- flask-api/blueprints/account.py
+++ flask-api/blueprints/account.py
@@ -5,45 +5,22 @@ from helpers import *
 
 account = Blueprint('account', __name__)
 
-@account.route('/register', methods=['POST', 'GET'])
-def register():
+@account.route('/account/login', methods=['POST', 'GET'])
+def accountLogin():
     content_type = request.headers.get('Content-Type')
-    table = Table('users', "email", "password", "first_name", "last_name", "account_value")
+    users = Table('users', "email", "first_name", "last_name", "account_value")
     if content_type == 'application/json':
         
         data = request.get_json()
         email = data['email']
-        password = data['password']
-        first_name =   data['first_name']
-        last_name = data['last_name']
+        first_name =   data['given_name']
+        last_name = data['family_name']
         account_value = 10000
 
         if isnewuser(email):
-            table.insert(email, password, first_name, last_name, account_value)
-            return jsonify({'message': 'User created successfully'})
+            users.insert(email, first_name, last_name, account_value)
+            return jsonify({"message": 'User created successfully'})
         else:
-            return jsonify({'message': 'User already exists'})
-    else:
-        return jsonify({'message': 'Please use POST method'})
-
-@account.route('/login', methods=['POST', 'GET'])
-def login():
-    users = Table('users', "email", "password", "first_name", "last_name", "account_value")
-    content_type = request.headers.get('Content-Type')
-    if content_type == 'application/json':
-        data = request.get_json()
-        email = data['email']
-        password = data['password']
-
-        user = users.getone('email', email)
-        accPass = user.get('password')
-
-        if accPass is None:
-            return jsonify({'message': 'User does not exist'})
-        else:
-            if accPass == password:
-                return jsonify({'message': 'Login successful'})
-            else:
-                return jsonify({'message': 'Incorrect password'})
+            return jsonify({'message': 'Returning user'})
     else:
         return jsonify({'message': 'Please use POST method'})

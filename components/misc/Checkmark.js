13b2e2dcfe7c698fd97d2590db482c5adf00a73c
sbelka-1703 <sbelka93@gmail.com>
2022-05-20 08:40:12 -0700

    Checkmark component added

---
+++ components/misc/Checkmark.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BsCheckLg } from "react-icons/bs";
+
+export const Checkmark = () => {
+    return (
+        <div className='flex'>
+            <BsCheckLg className='text-green-400 mr-1' />
+            <h1>Lorem epsum</h1>
+        </div>
+
+    )
+}

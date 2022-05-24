175751eae11a0f75ebfdd5c3407eea4e388c0d3e
sbelka-1703 <sbelka93@gmail.com>
2022-05-24 08:49:23 -0700

    improved upon MyCoin by InSight3D

--- pages/index.js
+++ pages/index.js
@@ -62,16 +62,16 @@ export default function Home() {
           <div className='flex  justify-around h-10 mb-[450px]'>
             {/* Text and input field */}
             <div className=' relative ml-52 space-y-8 mt-28'>
-              <p className='text-4xl font-extrabold w-[560px]'>MyCoin by InSight3D</p>
-              <p>The worlds first free to use mock Crypto Exchange with <br/>Academy and Supportive Community.</p>
+              <p className='text-8xl font-extrabold w-[560px]'>MyCoin by InSight3D</p>
+              <p className='text-2xl'>The worlds first free to use mock Crypto Exchange with <br/>Academy and Supportive Community.</p>
               <form className='flex items-center' action='/api/auth/login'>
                 <div className='relative flex items-center drop-shadow-lg text-gray-600 focus-within:text-gray-900'>
                   <MailIcon className="h-10 w-10 text-grey-400 absolute pointer-events-none pl-2" />
-                  <input className=' h-[58px] w-[300px] rounded-2xl pl-12 pr-3 font-semibold border-none ring-2 ring-gray-200'></input>
+                  <input className=' h-[80px] w-[450px] text-xl rounded-2xl pl-12 pr-3 font-semibold border-none ring-2 ring-gray-200'></input>
                 </div>
                 <div>
-                  <button className='bg-black h-[54px] right-[146px] rounded-2xl p-[13px] ml-2'>
-                    <span className='text-white text-[12px] font-extrabold'>
+                  <button className='bg-black h-[80px] w-[200px] right-[146px] rounded-2xl p-[13px] ml-2'>
+                    <span className='text-white text-[16px] font-extrabold'>
                       Get Started Today!
                     </span>
                   </button>

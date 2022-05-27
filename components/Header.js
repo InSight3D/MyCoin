d04dc86f2a8d53a24a289a51c9eefc6d0985e274
sbelka-1703 <sbelka93@gmail.com>
2022-05-27 11:38:19 -0700

    mobile nav started

--- components/Header.js
+++ components/Header.js
@@ -2,49 +2,51 @@ import Link from 'next/link'
 import axios from "axios";
 import React from 'react';
 import Image from 'next/image';
+import { GiHamburgerMenu } from "react-icons/gi";
 
-function login () {
+
+function login() {
   axios.post('http://localhost:5000/login', {
     email: 'neil@insight3d.tech',
     password: 'MyCoinAdmin123'
   })
-  .then(function (res) {
-    
-    const message = res.data.message
-    // check is responce equlas Login Successful
-    if (message === 'Incorrect password') {
-      console.log(message);
-    } else {
-      console.log(message);
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-    console.log("Login Failed");
-  });
- }
+    .then(function (res) {
+
+      const message = res.data.message
+      // check is responce equlas Login Successful
+      if (message === 'Incorrect password') {
+        console.log(message);
+      } else {
+        console.log(message);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+      console.log("Login Failed");
+    });
+}
 
 function Navigation() {
   return (
     /* navbar */
-    <header className='mr-24 ml-24'>
-      <div className="mx-auto px-4 mt-3 ">
+    <header className='ml-24 mr-24'>
+      <div className="px-4 mx-auto mt-3 ">
         <div className='flex justify-between '>
 
           {/* logo */}
-          <div className='flex space-x-4  '>
+          <div className='flex space-x-4 '>
             <Link href="/" passHref>
               <div className='flex space-x-2 cursor-pointer'>
                 <Image src="/logo.svg" width="80" height="50" alt="Logo" />
-                <a className='flex items-center py-5 px-2'>
-                  <span className='font-extrabold text-4xl'>MyCoin</span>
+                <a className='flex items-center px-2 py-5'>
+                  <span className='text-4xl font-extrabold'>MyCoin</span>
                 </a>
               </div>
             </Link>
           </div>
 
           {/* primary nav */}
-          <div className='flex items-center space-x-[46px] text-xl font-body-500 font-medium'>
+          <div className=' hidden md:flex md:items-center md:space-x-[46px] md:text-xl md:font-body-500 md:font-medium'>
             <div className='header-link group hover:text-gray-700'>
               <Link href="#academy" smooth={true} passHref>
                 <span className='span'>Academy</span>
@@ -63,17 +65,23 @@ function Navigation() {
           </div>
 
           {/* secondary nav */}
-          <div className=' flex items-center space-x-10 text-md '>
-            <form action="/dashboard">
-              <button type="submit" className=' py-2 px-4 rounded-2xl hover:bg-gray-200 btn btn-outline btn-accent m-1 font-semibold'>
+          
+          <div className='flex items-center space-x-10 text-md'>
+            
+            <form className = "hidden md:block" action="/dashboard">
+              <button  type="submit" className='px-4 py-2 m-1 font-semibold rounded-2xl hover:bg-gray-200 btn btn-outline btn-accent'>
                 Login
               </button>
             </form>
-            <form action="/dashboard">
-              <button type="submit" className='btn btn-outline btn-accent m-1 hover:bg-gray-700 bg-black text-white py-2 px-4 rounded-2xl font-medium '>
+            <form className = "hidden md:block" action="/dashboard">
+              <button type="submit" className='px-4 py-2 m-1 font-medium text-white bg-black btn btn-outline btn-accent hover:bg-gray-700 rounded-2xl '>
                 Get Started
               </button>
             </form>
+          </div>
+
+          <div className='flex items-center '>
+               <GiHamburgerMenu className='w-10 h-10 ml-96 md:hidden'/>
           </div>
         </div>
       </div>

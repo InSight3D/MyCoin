16eb7e885a486f8ea9496f0fa62c1c57fb18ebd0
Neil Shah <neildshah1013@gmail.com>
2022-05-31 08:14:55 -0400

    Merge with Main Branch

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,10 @@ import '../styles/globals.css'
 import { UserProvider } from '@auth0/nextjs-auth0';
 import React from 'react';
 import { useRouter } from "next/router";
+import { useEffect } from 'react';
 
-function MyApp({ Component, pageProps }) {
-  
+ function MyApp({ Component, pageProps }) {
+
   return (
     <Layout>
       <UserProvider>

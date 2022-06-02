0e138373ccad6539d8fb3ed3eff19ec54ad8df7b
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2022-06-02 13:53:06 -0400

    Test

--- pages/academy.js
+++ pages/academy.js
@@ -19,21 +19,36 @@ export default function dashboard() {
                 <meta name="twitter:image:alt" content="MyCoin" />
                 <link rel="icon" href="/logo.ico" />
             </Head>
-            <div className=' h-full '>
-            <div className='flex flex-col mb-80'>
-            <div className='flex relative h-10 mb-[450px]'>
-                {/* Text and input field */}
-                <div className=' relative ml-52 space-y-8 mt-28'>
-                <p className='text-4xl font-extrabold w-[560px]'>MyCoin Academy</p>
-                </div>
-
-                {/* Logo */}
-                <div className='absolute right-0 pr-52 mt-[85px]'>
-                <Image src="/logo.svg" width="269" height="272" alt="logo" />
+            <div class="flex justify-auto">
+            <div class="rounded-lg shadow-lg bg-white max-w-sm">
+                <a href="#!">
+                <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
+                </a>
+                <div class="p-6">
+                <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+                <p class="text-gray-700 text-base mb-4">
+                    Some quick example text to build on the card title and make up the bulk of the card's
+                    content.
+                </p>
+                <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
                 </div>
             </div>
             </div>
-        </div>
+            <div class="flex justify-auto">
+            <div class="rounded-lg shadow-lg bg-white max-w-sm">
+                <a href="#!">
+                <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
+                </a>
+                <div class="p-6">
+                <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+                <p class="text-gray-700 text-base mb-4">
+                    Some quick example text to build on the card title and make up the bulk of the card's
+                    content.
+                </p>
+                <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+                </div>
+            </div>
+            </div>
         </>
     )
 }

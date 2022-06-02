261fcb912c560078557318be05f73fe2f8c52365
Neil Shah <78612612+NeilShah2026@users.noreply.github.com>
2022-06-02 13:55:35 -0400

    className

--- pages/academy.js
+++ pages/academy.js
@@ -19,33 +19,18 @@ export default function dashboard() {
                 <meta name="twitter:image:alt" content="MyCoin" />
                 <link rel="icon" href="/logo.ico" />
             </Head>
-            <div class="flex justify-auto">
-            <div class="rounded-lg shadow-lg bg-white max-w-sm">
+            <div className="flex justify-auto">
+            <div className="rounded-lg shadow-lg bg-white max-w-sm">
                 <a href="#!">
-                <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
+                <img className="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
                 </a>
-                <div class="p-6">
-                <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
-                <p class="text-gray-700 text-base mb-4">
+                <div className="p-6">
+                <h5 className="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+                <p className="text-gray-700 text-base mb-4">
                     Some quick example text to build on the card title and make up the bulk of the card's
                     content.
                 </p>
-                <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
-                </div>
-            </div>
-            </div>
-            <div class="flex justify-auto">
-            <div class="rounded-lg shadow-lg bg-white max-w-sm">
-                <a href="#!">
-                <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
-                </a>
-                <div class="p-6">
-                <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
-                <p class="text-gray-700 text-base mb-4">
-                    Some quick example text to build on the card title and make up the bulk of the card's
-                    content.
-                </p>
-                <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+                <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
                 </div>
             </div>
             </div>

5c0e2a172fd0b765388d8ea349cb44a6cc42d7e6
sbelka-1703 <sbelka93@gmail.com>
2022-06-19 12:25:14 -0700

    intial research page complete

--- pages/research.js
+++ pages/research.js
@@ -3,6 +3,7 @@ import { useUser } from "@auth0/nextjs-auth0";
 import Head from "next/head";
 import Image from "next/image";
 import { GrSearch, GrBitcoin } from "react-icons/gr";
+import { RiCopperCoinLine } from "react-icons/ri";
 
 // import next/link
 
@@ -29,25 +30,35 @@ export default function dashboard() {
       </Head>
 
       <div>
-        <div className="relative block ml-10 mr-10 md:hidden">
+        <div className="relative block mb-4 ml-10 mr-10 md:hidden">
           <input className="w-full h-20 text-4xl border-2 border-black rounded-3xl pl-14"></input>
           <GrSearch className="absolute text-4xl top-6 left-4" />
         </div>
         <div className="grid grid-cols-4 mb-10 ml-10 ">
-          <div className="hidden bg-gray-600 border-2 rounded-2xl md:block">
+          <div className="hidden bg-gray-300 rounded-2xl md:block">
             {/* Searchbar Dektop */}
             <div className="relative hidden mt-4 ml-10 mr-10 md:block">
               <input className="w-full text-2xl border-2 border-black pl-[50px] h-14 rounded-3xl"></input>
               <GrSearch className="absolute text-4xl top-[12px] left-3" />
             </div>
             {/* Price feed Desktop */}
-            <div>
-              <div></div>
-              <div></div>
-              <div></div>
+            <div className="grid grid-cols-1 place-items-center gap-y-2">
+              <p className="mt-6 font-bold">Top 10 trading assets</p>
+              <div className="flex items-center px-4 py-2 bg-white rounded-2xl">
+                <RiCopperCoinLine className="mr-4 text-4xl" />
+                <p className="text-2xl font-bold ">Price: $10,000</p>
+              </div>
+              <div className="flex items-center px-4 py-2 bg-white rounded-2xl">
+                <RiCopperCoinLine className="mr-4 text-4xl" />
+                <p className="text-2xl font-bold ">Price: $10,000</p>
+              </div>
+              <div className="flex items-center px-4 py-2 bg-white rounded-2xl">
+                <RiCopperCoinLine className="mr-4 text-4xl" />
+                <p className="text-2xl font-bold ">Price: $10,000</p>
+              </div>
             </div>
           </div>
-          <div className="grid content-center justify-start col-span-3 text-lg font-extrabold border-2 grid-custom-mobile md:grid-custom gap-y-10 gap-x-5 ml-14">
+          <div className="grid content-center justify-start col-span-3 text-lg font-extrabold grid-custom-2 md:grid-custom-3 gap-y-10 gap-x-5 ml-14">
             <div className="px-12 pt-6 pb-6 bg-gray-300 h-fill py-14 rounded-3xl">
               <div className="flex flex-col items-center justify-between">
                 <div className="flex flex-col items-center">
@@ -123,24 +134,6 @@ export default function dashboard() {
           </div>
         </div>
       </div>
-      {/* <div className="h-full ">
-      </div>
-        <div className="flex flex-col mb-80">
-          <div className="flex relative h-10 mb-[450px]">
-            // Text and input field
-            <div className="relative space-y-8 ml-52 mt-28">
-              <p className="text-4xl font-extrabold w-[560px]">
-                MyCoin Reaserch
-              </p>
-            </div>
-
-            // Logo
-            <div className="absolute right-0 pr-52 mt-[85px]">
-              <Image src="/logo.svg" width="269" height="272" alt="logo" />
-            </div>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 }
